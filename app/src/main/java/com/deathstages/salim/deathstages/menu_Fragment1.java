33243ac62a9ee7e2a96f383ecce82cc1f88d8e2c
Salim Elayan <Selayan@gmail.com>
2015-03-16 21:22:20 -0400

    Removed Listview

--- app/src/main/java/com/deathstages/salim/deathstages/menu_Fragment1.java
+++ app/src/main/java/com/deathstages/salim/deathstages/menu_Fragment1.java
@@ -1,10 +1,16 @@
 package com.deathstages.salim.deathstages;
 
+import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.deathstages.salim.deathstages.ListViewActivity;
 
 /**
  * Created by Salim on 3/11/2015.
@@ -19,4 +25,5 @@ public class menu_Fragment1 extends android.app.Fragment {
         rootview = inflater.inflate(R.layout.menu_layout1, container, false);
         return rootview;
     }
+    private ListViewActivity activity = new ListViewActivity();
 }
